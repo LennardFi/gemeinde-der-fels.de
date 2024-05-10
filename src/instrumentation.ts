2a2d2b58ee02a84375ff6a8558ac3216fe324234
LennardFi <info@lennard-fickus.de>
2024-05-10 03:27:55 +0200

    fixed instrumentation script for production

--- src/instrumentation.ts
+++ src/instrumentation.ts
@@ -1,57 +1,64 @@
 import { configuredFeatureFlags, isDevMode } from "./lib/shared/develop"
+import { readEnvValueSafely } from "./lib/shared/env"
 import { WebsiteError } from "./lib/shared/errors"
 
-export async function register() {
-    try {
-        console.log(
-            `Configured feature flags:\n${configuredFeatureFlags
-                .map((flag) => `\t- ${flag}`)
-                .join("\n")}`,
-        )
+const DB_DATABASE_VERSION: number = 1
 
-        if (isDevMode) {
-            if (process.env.NEXT_RUNTIME === "nodejs") {
+export async function register() {
+    if (process.env.NEXT_RUNTIME === "nodejs") {
+        try {
+            console.log(
+                `Configured feature flags:\n${configuredFeatureFlags
+                    .map((flag) => `\t- ${flag}`)
+                    .join("\n")}`,
+            )
+
+            const { getClient } = await import("./lib/backend/databaseHelpers")
+
+            let dbMetadata = await getClient().databaseMetadata.findFirst()
+            let databaseInitialized: boolean = false
+
+            if (dbMetadata === null) {
+                dbMetadata = await getClient().databaseMetadata.create({
+                    data: {
+                        version: DB_DATABASE_VERSION,
+                        isDevData: isDevMode,
+                    },
+                })
+            } else {
+                databaseInitialized = true
+            }
+
+            if (isDevMode) {
                 const { setupTestEnv } = await import("../test/setupTestEnv")
 
-                const { getClient } = await import(
-                    "./lib/backend/databaseHelpers"
-                )
-
-                const dbMetadata =
-                    await getClient().databaseMetadata.findFirst()
-
-                if (dbMetadata !== null) {
-                    if (!dbMetadata.isDevData) {
-                        return
-                    }
-
-                    if (
-                        process.env[
-                            "GDF_DEV_DATABASE_REPLACE_EXISTING_DATA"
-                        ] !== "1"
-                    ) {
-                        return
-                    }
+                if (
+                    !databaseInitialized ||
+                    (isDevMode &&
+                        readEnvValueSafely(
+                            "GDF_DEV_DATABASE_REPLACE_EXISTING_DATA",
+                            "boolean",
+                        ))
+                ) {
+                    setupTestEnv(true)
                 }
-
-                setupTestEnv(true)
             }
-        }
-    } catch (e) {
-        if (e instanceof WebsiteError) {
-            console.error(e.toLogOutput())
-            return
-        }
+        } catch (e) {
+            if (e instanceof WebsiteError) {
+                console.error(e.toLogOutput())
+                return
+            }
 
-        console.error(
-            new WebsiteError(
-                "build",
-                "Unknown error while running instrumentation hook",
-                {
-                    internalException: e instanceof Error ? e : undefined,
-                    internalMessage: JSON.stringify(e),
-                },
-            ).toLogOutput(),
-        )
+            console.error(
+                new WebsiteError(
+                    "build",
+                    "Unknown error while running instrumentation hook",
+                    {
+                        internalException: e instanceof Error ? e : undefined,
+                        internalMessage: JSON.stringify(e),
+                    },
+                ).toLogOutput(),
+            )
+        }
     }
 }

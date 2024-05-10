9ba25b69fcec5cd2824642bb900012d6a236cf6e
LennardFi <info@lennard-fickus.de>
2024-05-10 13:18:59 +0200

    added fallback page for use when next app is rebuilding

--- next.config.js
+++ next.config.js
@@ -22,6 +22,20 @@ const nextConfig = {
     experimental: {
         instrumentationHook: true,
     },
+    async headers() {
+        return [
+            {
+                // matching all API routes
+                source: "/",
+                headers: [
+                    { key: "Access-Control-Allow-Credentials", value: "true" },
+                    { key: "Access-Control-Allow-Origin", value: "*" },
+                    { key: "Access-Control-Allow-Methods", value: "GET" },
+                    { key: "Access-Control-Allow-Headers", value: "*" },
+                ],
+            },
+        ]
+    },
 }
 
 module.exports = nextConfig

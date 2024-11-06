15aa5c622b9bb8ebeea58d1c21c46a1b7a615694
LennardFi <info@lennard-fickus.de>
2024-11-06 05:32:07 +0100

    WIP: fixing playwright tests

--- e2e/pages/index.spec.ts
+++ e2e/pages/index.spec.ts
@@ -3,8 +3,5 @@ import { expect, test } from "@playwright/test"
 test(`Check "Index" page behavior`, async ({ page }) => {
     await page.goto("/")
 
-    // Page should have "Portfolio" navigation entry selected
-    await expect(page.locator(".Navigation_currentPage__B_txF")).toContainText(
-        "Portfolio"
-    )
+    await expect(page.locator("h1")).toContainText("Wir sindGemeindeder Fels")
 })
--- e2e/pages/kontakt.spec.ts
+++ e2e/pages/kontakt.spec.ts
@@ -2,10 +2,8 @@ import { expect, test } from "@playwright/test"
 
 test(`Check "Contact" page behavior`, async ({ page }) => {
     // Start from the index page (the baseURL is set via the webServer in the playwright.config.ts)
-    await page.goto("/contact")
+    await page.goto("/kontakt")
 
     // Page should have "Kontakt" navigation entry selected
-    await expect(page.locator(".Navigation_currentPage__B_txF")).toContainText(
-        "Kontakt"
-    )
+    await expect(page.locator("h1")).toContainText("Kontakt")
 })
